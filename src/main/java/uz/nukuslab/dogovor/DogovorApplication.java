8a52a70c4f3df2963fd9aac837d67aa9d72fbda2
saylaww <saylaww@gmail.com>
2022-04-05 11:21:31 +0500

    heroku home page

--- src/main/java/uz/nukuslab/dogovor/DogovorApplication.java
+++ src/main/java/uz/nukuslab/dogovor/DogovorApplication.java
@@ -2,9 +2,20 @@ package uz.nukuslab.dogovor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class DogovorApplication {
+
+    @GetMapping
+    public String home(){
+        return "Dogovor app home page";
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(DogovorApplication.class, args);
