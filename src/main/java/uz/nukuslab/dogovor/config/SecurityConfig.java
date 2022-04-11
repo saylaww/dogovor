b1dd03e70c47d843fb555dcdce00e3d137eed576
saylaww <saylaww@gmail.com>
2022-04-11 11:46:58 +0500

    swagger doc #8

--- src/main/java/uz/nukuslab/dogovor/config/SecurityConfig.java
+++ src/main/java/uz/nukuslab/dogovor/config/SecurityConfig.java
@@ -50,7 +50,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/login",
-                        "/")
+                        "/",
+                        "/swagger-ui/index.html")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
